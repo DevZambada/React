c60d0225545b3dabe25fb65c67049e3bb2820cc1
MaxZambada <max.zambada@hotmail.com>
2023-02-21 09:14:53 -0600

    Start of Project 1 in React

--- Recursos/FundamentosJS-FIN/js/30.js
+++
@@ -1,9 +0,0 @@
-// Fetch API  - Async Await
-
-const url = "https://jsonplaceholder.typicode.com/comments"
-
-const consultarAPI = async () => {
-    const respuesta = await fetch(url)
-    const resultado = await respuesta.json()
-}
-consultarAPI();
---
+++ citas-react/src/App.jsx
@@ -0,0 +1,12 @@
+
+function App() {
+
+  return (
+    <div>
+      <h1>Hola Mundo</h1>
+      <p>Un Párrafo</p>
+    </div>
+  )
+}
+
+export default App
