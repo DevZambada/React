e2b9fd100f5063e053f83961880a3900b6645ec3
DevZambada <max.zambadac@outlook.com>
2023-03-08 14:23:01 -0600

    branch for experimentation

--- citas-react/src/components/Formulario.jsx
+++ citas-react/src/components/Formulario.jsx
@@ -164,3 +164,137 @@ const Formulario = ({ pacientes, setPacientes }) => {
 };
 
 export default Formulario;
+
+/*
+import { useState, useEffect } from "react";
+import Error from "./Error";
+import FormInput from "./FormInput";
+
+const Formulario = ({ pacientes, setPacientes }) => {
+  const [form, setForm] = useState({
+    nombre: "",
+    propietario: "",
+    email: "",
+    fechaDeAlta: "",
+    sintomas: "",
+  });
+
+  const { nombre, propietario, email, fechaDeAlta, sintomas } = form;
+
+  const [error, setError] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(e);
+
+    //Validación de formulario
+    // if ([nombre, propietario, email, fechaDeAlta, sintomas].includes("")) {
+    //   console.log("Hay al menos un campo vacio");
+
+    //   setError(true);
+    //   return;
+    // }
+
+    setError(false);
+
+    //objeto de Paciente
+    const objetoPaciente = {
+      nombre,
+      propietario,
+      email,
+      fechaDeAlta,
+      sintomas,
+    };
+
+    setPacientes([...pacientes, objetoPaciente]);
+
+    //Reiniciar el formulario
+    setForm({
+      nombre: "",
+      propietario: "",
+      email: "",
+      fechaDeAlta: "",
+      sintomas: "",
+    });
+  };
+
+  const handleChange = (event) => {
+    setForm({
+      ...form,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  return (
+    <div className="md:w-1/2 lg:w-2/5 mx-5">
+      <h2 className="font-black text-3xl text-center">Seguimiento Pacientes</h2>
+
+      <p className="text-lg mt-5 text-center mb-10">
+        Añade Pacientes y {""}
+        <span className="text-indigo-600 font-bold">Adminístralos</span>
+      </p>
+
+      <form
+        onSubmit={handleSubmit}
+        className="bg-white shadow-md rounded-lg py-10 px-5 mb-10"
+      >
+        {error && (
+          <Error>
+            <p>Todos los campos son obligatorios</p>
+          </Error>
+        )}
+        <FormInput
+          name={"mascota"}
+          id={"mascota"}
+          type={"text"}
+          onChange={handleChange}
+          value={nombre}
+          placeholder={"Nombre de la mascota"}
+        />
+        <FormInput
+          name={"propietario"}
+          id={"propietario"}
+          type={"text"}
+          value={propietario}
+          onChange={handleChange}
+          placeholder={"Nombre del propietario"}
+        />
+        <FormInput
+          name={"email"}
+          id={"email"}
+          type={"email"}
+          value={email}
+          onChange={handleChange}
+          placeholder={"Email"}
+        />
+        <FormInput
+          name={"fechaDeAlta"}
+          id={"fechaDeAlta"}
+          type={"date"}
+          value={fechaDeAlta}
+          onChange={handleChange}
+          placeholder={"Fecha de alta"}
+        />
+        <FormInput
+          name={"sintomas"}
+          id={"sintomas"}
+          type={"text"}
+          value={sintomas}
+          onChange={handleChange}
+          placeholder={"Sintomas"}
+          classes={"block"}
+        />
+        <button
+          type="submit"
+          className="bg-indigo-600 w-full p-3 text-white uppercase font-bold
+             hover:bg-indigo-700 cursor-pointer transition-colors"
+        >
+          Agregar paciente
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Formulario;
+*/
