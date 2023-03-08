bb9a3107d8c7a6ded83a60dabd345a3b9131e36a
DevZambada <max.zambadac@outlook.com>
2023-03-08 14:51:55 -0600

    Added Edit and Remove buttons

--- citas-react/src/App.jsx
+++ citas-react/src/App.jsx
@@ -5,14 +5,19 @@ import ListadoPacientes from "./components/ListadoPacientes";
 
 function App() {
   const [pacientes, setPacientes] = useState([]);
+  const [paciente, setPaciente] = useState([]);
 
   return (
     <div className="container mx-auto mt-20">
       <Header />
 
       <div className="mt-12 md:flex ">
-        <Formulario setPacientes={setPacientes} pacientes={pacientes} />
-        <ListadoPacientes pacientes={pacientes} />
+        <Formulario
+          setPacientes={setPacientes}
+          pacientes={pacientes}
+          paciente={paciente}
+        />
+        <ListadoPacientes pacientes={pacientes} setPaciente={setPaciente} />
       </div>
     </div>
   );
