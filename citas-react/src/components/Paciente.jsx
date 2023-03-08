ce2854f66cef8d26d474e12e5415f31a76035f9d
DevZambada <max.zambadac@outlook.com>
2023-03-08 09:56:36 -0600

    Using props to modify ListaPacientes as well as conditional formatting

--- citas-react/src/components/Paciente.jsx
+++ citas-react/src/components/Paciente.jsx
@@ -1,30 +1,27 @@
-const Paciente = () => {
+const Paciente = ({ paciente }) => {
+  const { nombre, propietario, email, fecha, sintomas } = paciente;
+
   return (
     <div className="mx-5 my-10 bg-white shadow-md px-5 py-10 rounded-xl">
       <p className="font-bold mb-3 uppercase text-gray-700">
         Nombre: {""}
-        <span className="font-normal normal-case">Hook</span>
+        <span className="font-normal normal-case">{paciente.nombre}</span>
       </p>
       <p className="font-bold mb-3 uppercase text-gray-700">
         Propietario: {""}
-        <span className="font-normal normal-case">Juan</span>
+        <span className="font-normal normal-case">{paciente.propietario}</span>
       </p>
       <p className="font-bold mb-3 uppercase text-gray-700">
         Email: {""}
-        <span className="font-normal normal-case">correo@correo.com</span>
+        <span className="font-normal normal-case">{paciente.email}</span>
       </p>
       <p className="font-bold mb-3 uppercase text-gray-700">
         Fecha Alta: {""}
-        <span className="font-normal normal-case">10 Diciembre de 2022</span>
+        <span className="font-normal normal-case">{paciente.fecha}</span>
       </p>
       <p className="font-bold mb-3 uppercase text-gray-700">
         Síntomas: {""}
-        <span className="font-normal normal-case">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro minim
-          earum reiciendis autem dolorum totam recusandae ullam obcaecat tempore
-          natus neque, dolor maxime a quos! Natus voluptatum commodi consequatur
-          cupiditate.
-        </span>
+        <span className="font-normal normal-case">{paciente.sintomas}</span>
       </p>
     </div>
   );
